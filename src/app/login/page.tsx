0e631bc428c2d1c4e18c6455325aaf44cd84e224
AditiSaxena <saxena.aditi2805@gmail.com>
2024-09-17 00:57:39 +0530

    changesmade

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,14 +5,15 @@ import { z } from "zod";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';  // Import the CSS file for Toastify
 
+// Validation schema using Zod
 const loginSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
   password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
 });
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -35,21 +36,26 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100 p-4 relative">
-      <main className="w-[375px] bg-transparent p-6 rounded-lg">
-        <h1 className="text-2xl font-bold text-center mb-6 text-black">Login</h1>
+    <div className="flex flex-col justify-center items-center min-h-screen bg-white p-4">
+      <main className="w-full max-w-md bg-white p-6 rounded-lg mt-[-50px]" style={{ fontFamily: 'Inter, sans-serif' }}>
+        <h1 
+          className="text-2xl font-bold text-center mb-6 text-black"
+          style={{ fontFamily: 'Inter, sans-serif' }}>
+          Login
+        </h1>
+        
         <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           
           {/* VIT Mail Input */}
-          <div>
-            <label htmlFor="email" className="text-sm font-bold text-black">
+          <div className="flex flex-col mb-4">
+            <label htmlFor="email" className="text-sm font-semibold text-black mb-1">
               Enter VIT Mail
             </label>
             <input
               id="email"
               type="email"
               placeholder="VIT email ID"
-              className="w-full p-3 mt-1 text-lg border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+              className="w-full h-[50px] px-3 text-lg bg-gray-200 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
@@ -57,20 +63,20 @@ export default function LoginPage() {
           </div>
 
           {/* Password Input */}
-          <div className="relative">
-            <label htmlFor="password" className="text-sm font-bold text-black">
+          <div className="flex flex-col mb-4">
+            <label htmlFor="password" className="text-sm font-semibold text-black mb-1">
               Enter Password
             </label>
             <input
               id="password"
               type="password"
               placeholder="Password"
-              className="w-full p-3 mt-1 text-lg border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+              className="w-full h-[50px] px-3 text-lg bg-gray-200 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
-            <p className="text-xs font-bold text-gray-500 mt-1">
+            <p className="text-xs font text-gray-500 mt-1">
               We've sent your login credentials to your registered email.
             </p>
           </div>
@@ -78,7 +84,7 @@ export default function LoginPage() {
           {/* Login Button */}
           <button
             type="submit"
-            className="w-full p-3 mt-4 text-lg font-semibold text-white rounded-lg bg-[#FBB3C0] hover:bg-pink-400 focus:outline-none focus:ring-2 focus:ring-pink-600"
+            className="w-full h-[50px] px-3 text-lg font-semibold text-black rounded-lg bg-[#FBB3C0] hover:bg-pink-400 focus:outline-none focus:ring-2 focus:ring-pink-600"
           >
             Login
           </button>
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { z } from 'zod';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Copy, RefreshCw, Share2 } from 'lucide-react'; // Import Lucide icons
+import { Copy } from 'lucide-react'; // Import Lucide icons
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -54,113 +54,132 @@ export default function Home() {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-50 p-4">
-      <form onSubmit={handleSubmit(onSubmit)} className="bg-white p-6 rounded-lg w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-6 text-center">
-          {isTeamCreated ? 'Team Formation' : 'Join an Existing Team'}
+    <div className="flex flex-col items-center min-h-screen bg-white p-4">
+      <form onSubmit={handleSubmit(onSubmit)} className="bg-white p-6 rounded-lg w-full max-w-md mt-16 " style={{ padding: '24px', fontFamily: 'Inter, sans-serif' }}>
+        {/* Static Heading */}
+        <h1
+          className="text-center mb-6"
+          style={{
+            fontSize: '24px',
+            fontWeight: '700',
+            lineHeight: '29.05px',
+            textAlign: 'center',
+          }}
+        >
+          Team Formation
         </h1>
-
+        
         {/* Create New Team Toggle */}
-        <div className="flex justify-center mb-6 items-center">
-          <label className="inline-flex items-center cursor-pointer">
-            <span className="mr-3 text-gray-700 text-sm">
-              {isTeamCreated ? 'Team Leader makes a new team' : 'Join an existing team'}
-            </span>
-            <div
-              className={`relative inline-flex items-center cursor-pointer ${
-                isTeamCreated ? 'bg-green-500' : 'bg-gray-200'
-              } rounded-full w-12 h-6 transition-colors`}
-              onClick={() => setIsTeamCreated((prevState) => !prevState)}
-            >
+        <div className="flex flex-col items-center mb-6">
+          <span
+            className="text-lg font-semibold mb-2"
+            style={{
+              fontSize: '16px',
+              fontWeight: '400',
+              lineHeight: '19.36px',
+              textAlign: 'left',
+            }}
+          >
+            Create a new team?
+          </span>
+          <div className="flex items-center">
+            <label className="inline-flex items-center cursor-pointer">
               <div
-                className={`absolute left-0 top-0 w-6 h-6 bg-white rounded-full transition-transform ${
-                  isTeamCreated ? 'translate-x-6' : ''
+                className={`relative inline-flex items-center cursor-pointer w-10 h-5 rounded-full border border-gray-300 ${
+                  isTeamCreated ? 'bg-[#AEF276]' : 'bg-gray-200'
                 }`}
-              />
-              <div
-                className={`absolute left-0 top-0 w-6 h-6 flex items-center justify-center transition-transform ${
-                  isTeamCreated ? 'translate-x-6' : ''
-                }`}
+                onClick={() => setIsTeamCreated((prevState) => !prevState)}
               >
-                {isTeamCreated ? (
-                  <span className="text-green-600 text-xs">✓</span>
-                ) : (
-                  <span className="text-red-600 text-xs">✗</span>
-                )}
+                <div
+                  className={`absolute left-0 top-0 w-5 h-5 bg-white rounded-full transition-transform ${
+                    isTeamCreated ? 'translate-x-5' : ''
+                  }`}
+                />
               </div>
-            </div>
-          </label>
+            </label>
+            <span className="ml-4 text-lg">
+              {isTeamCreated ? 'Yes' : 'No'}
+            </span>
+          </div>
+          <div className="mt-2">
+            <span
+              className="text-gray-500"
+              style={{
+                fontSize: '14px',
+                fontWeight: '400',
+                lineHeight: '16.94px',
+                textAlign: 'center',
+              }}
+            >
+              {isTeamCreated ? 'Team Leader makes the New Team' : 'Join an Existing Team'}
+            </span>
+          </div>
         </div>
 
         {/* Conditionally render forms based on toggle */}
         {isTeamCreated ? (
           <>
             {/* Team Name */}
-            <div className="mb-6">
+            <div className="mb-4">
+              <label htmlFor="teamName" className="font-semibold text-gray-700 text-sm">Team Name</label>
               <input
                 {...register("teamName")}
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                className="w-full px-4 py-2 bg-gray-200 border-none rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
                 placeholder="Enter team name"
               />
               {errors.teamName && (
-                <p className="text-red-500 text-sm mt-1">{errors.teamName.message}</p>
+                <p className="text-red-500 text-xs mt-1">{errors.teamName.message}</p>
               )}
             </div>
 
             {/* Team Code */}
-            <div className="mb-6">
-              <div className="mb-2 text-gray-600">Your teammates can use this code to join you!</div>
-              <div className="flex items-center">
+            <div className="mb-4">
+              <label htmlFor="teamCode" className="font-semibold text-gray-700 text-sm">Team Code</label>
+              <p className="text-gray-600 text-sm mb-2" style={{ fontSize: '14px', fontWeight: '300', lineHeight: '16.94px', textAlign: 'left' }}>
+                Your teammates can use this code to join you!
+              </p>
+              <div className="relative">
                 <input
                   {...register("teamCode")}
-                  className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                  className="w-full px-4 py-2 bg-gray-200 border-none rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
                   placeholder="Team code"
                   value={teamCode}
                   readOnly
                 />
-                <div className="flex ml-2 space-x-2">
-                  <button
-                    type="button"
-                    className="p-2 bg-gray-200 rounded hover:bg-gray-300 flex items-center justify-center"
-                    onClick={handleCopyToClipboard}
-                  >
-                    <Copy className="text-gray-600 text-lg" />
-                  </button>
-                  <button
-                    type="button"
-                    className="p-2 bg-gray-200 rounded hover:bg-gray-300 flex items-center justify-center"
-                    onClick={() => setTeamCode(generateTeamCode())}
-                  >
-                    <RefreshCw className="text-gray-600 text-lg" />
-                  </button>
-                  <button
-                    type="button"
-                    className="p-2 bg-gray-200 rounded hover:bg-gray-300 flex items-center justify-center"
-                  >
-                    <Share2 className="text-gray-600 text-lg" />
-                  </button>
-                </div>
+                <button
+                  type="button"
+                  className="absolute right-2 top-1/2 transform -translate-y-1/2 p-2 bg-gray-200 rounded hover:bg-gray-300"
+                  onClick={handleCopyToClipboard}
+                >
+                  <Copy className="text-gray-600 text-lg" />
+                </button>
               </div>
-              {errors.teamCode && <p className="text-red-500 text-sm mt-1">{errors.teamCode.message}</p>}
+              {errors.teamCode && <p className="text-red-500 text-xs mt-1">{errors.teamCode.message}</p>}
             </div>
           </>
         ) : (
-          <div className="mb-6">
+          <div className="mb-4">
+            <label htmlFor="teamCode" className="font-semibold text-gray-700 text-sm">Join a team</label>
+            <p className="text-gray-500 text-sm mb-2" style={{ fontSize: '14px', fontWeight: '300', lineHeight: '16.94px', textAlign: 'left' }}>
+              Enter the code from your team to join them!
+            </p>
             <input
               {...register("teamCode")}
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              className="w-full px-4 py-2 bg-gray-200 border-none rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
               placeholder="Enter team code"
             />
-            {errors.teamCode && <p className="text-red-500 text-sm mt-1">{errors.teamCode.message}</p>}
+            {errors.teamCode && <p className="text-red-500 text-xs mt-1">{errors.teamCode.message}</p>}
           </div>
         )}
 
         {/* Submit Button */}
         <button
           type="submit"
-          className="w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600"
+          className={`w-full py-2 rounded-lg font-semibold ${
+            isTeamCreated ? 'bg-[#AEF276] text-black hover:bg-green-700' : 'bg-[#88DBF9] text-black hover:bg-blue-700'
+          }`}
         >
-          {isTeamCreated ? 'Create team' : 'Join team'}
+          {isTeamCreated ? 'Create Team' : 'Join Team'}
         </button>
       </form>
       <ToastContainer />
