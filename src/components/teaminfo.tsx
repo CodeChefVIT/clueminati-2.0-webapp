09056537979446da6e1eb663c3fb31852bc0c5b0
Abh1noob <abhinavganeshank@gmail.com>
2024-09-16 22:10:48 +0530

    fix: build

--- src/components/teaminfo.tsx
+++ src/components/teaminfo.tsx
@@ -1,46 +1,49 @@
-import React from 'react';
-import {Progress} from '@/components/ui/progress';
-import icon1 from "@/assets/images/ico1.svg"
-import icon2 from "@/assets/images/ico2.svg"
-import circ from "@/assets/images/circle.svg"
+import circ from "@/assets/images/circle.svg";
+import icon1 from "@/assets/images/ico1.svg";
+import icon2 from "@/assets/images/ico2.svg";
+import { Progress } from "@/components/ui/progress";
 import Image from "next/image";
-interface TeamInfoProps {}
 
-const TeamInfo: React.FC<TeamInfoProps> = () => {
+const TeamInfo = () => {
   return (
-    <section className="flex flex-col self-stretch pb-2.5 mt-3 w-full custom-yellow rounded-2xl">
+    <section className="custom-yellow mt-3 flex w-full flex-col self-stretch rounded-2xl pb-2.5">
       <div className="flex self-start text-base font-medium">
         <Image
-          loading="lazy"
-          src={circ}
+          src={circ as HTMLImageElement}
           alt="Team avatar"
-          className="object-contain shrink-0 aspect-square shadow-[0px_1px_24px_rgba(216,229,237,0.18)] w-[130px]"
+          height={1000}
+          width={1000}
+          className="aspect-square w-[130px] shrink-0 object-contain shadow-[0px_1px_24px_rgba(216,229,237,0.18)]"
         />
-        <div className="flex flex-col items-start my-auto">
-          <h2 className="self-stretch font-bold text-xl text-black">Team Name</h2>
+        <div className="my-auto flex flex-col items-start">
+          <h2 className="self-stretch text-xl font-bold text-black">
+            Team Name
+          </h2>
           <p className="text-black">400 Points</p>
-          <p className="mt-1 text-xs  text-zinc-600">Bronze Tier</p>
+          <p className="mt-1 text-xs text-zinc-600">Bronze Tier</p>
         </div>
       </div>
-      <div className="flex flex-col px-3 w-full">
+      <div className="flex w-full flex-col px-3">
         <Progress />
-        <div className="flex gap-2 mt-2.5 text-xs font-medium text-zinc-800">
+        <div className="mt-2.5 flex gap-2 text-xs font-medium text-zinc-800">
           <div className="flex flex-auto gap-1">
             <Image
-              loading="lazy"
-              src={icon1}
+              src={icon1 as HTMLImageElement}
               alt=""
-              className="object-contain shrink-0 aspect-square w-[61px]"
+              height={1000}
+              width={1000}
+              className="aspect-square w-[61px] shrink-0 object-contain"
             />
             <p className="my-auto">
               Minimum requirement for silver tier currently is 600 points
             </p>
           </div>
           <Image
-            loading="lazy"
-            src={icon2}
+            src={icon2 as HTMLImageElement}
             alt=""
-            className="object-contain shrink-0 my-auto w-18 aspect-square shadow-[0px_1px_13px_rgba(216,229,237,0.18)]"
+            height={1000}
+            width={1000}
+            className="w-18 my-auto aspect-square shrink-0 object-contain shadow-[0px_1px_13px_rgba(216,229,237,0.18)]"
           />
         </div>
       </div>
