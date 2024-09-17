1dfc5d2afd8c44ec69e5f11018944c460824362b
ruhi-doshi <ruhidoshi05@gmail.com>
2024-09-17 11:58:19 +0530

    merge conflicts resolved

--- src/components/teaminfo.tsx
+++ src/components/teaminfo.tsx
@@ -5,32 +5,34 @@ import icon2 from "@/assets/images/ico2.svg";
 import circ from "@/assets/images/circle.svg";
 import Image from "next/image";
 
-interface TeamInfoProps {}
-
-const TeamInfo: React.FC<TeamInfoProps> = () => {
+const TeamInfo = () => {
   return (
-    <section className="flex flex-col self-stretch pb-2.5 mt-3 w-full bg-customYellow rounded-2xl">
+    <section className="flex flex-col self-stretch pb-2.5 mt-3 w-full bg-customYellow rounded-2xl custom-yellow">
       <div className="flex self-start text-base font-medium">
         <Image
-          loading="lazy"
-          src={circ}
+          src={circ as HTMLImageElement}
           alt="Team avatar"
-          className="object-contain shrink-0 aspect-square shadow-[0px_1px_24px_rgba(216,229,237,0.18)] w-[130px]"
+          height={1000}
+          width={1000}
+          className="aspect-square w-[130px] shrink-0 object-contain shadow-[0px_1px_24px_rgba(216,229,237,0.18)]"
         />
-        <div className="flex flex-col items-start my-auto">
-          <h2 className="self-stretch font-bold text-xl text-black">Team Name</h2>
+        <div className="my-auto flex flex-col items-start">
+          <h2 className="self-stretch text-xl font-bold text-black">
+            Team Name
+          </h2>
           <p className="text-black">400 Points</p>
           <p className="mt-1 text-xs text-zinc-600">Bronze Tier</p>
         </div>
       </div>
-      <div className="flex flex-col px-3 w-full">
+      <div className="flex w-full flex-col px-3">
         <Progress />
         <div className="flex justify-between mt-2.5 text-xs font-medium text-zinc-800 w-full">
           <div className="flex gap-2 items-center">
             <Image
-              loading="lazy"
-              src={icon1}
+              src={icon1 as HTMLImageElement}
               alt=""
+              height={1000}
+              width={1000}
               className="object-contain shrink-0 aspect-square w-[40px] sm:w-[61px]"
             />
             <p>
@@ -38,9 +40,10 @@ const TeamInfo: React.FC<TeamInfoProps> = () => {
             </p>
           </div>
           <Image
-            loading="lazy"
-            src={icon2}
+            src={icon2 as HTMLImageElement}
             alt=""
+            height={1000}
+            width={1000}
             className="object-contain shrink-0 aspect-square w-[50px] sm:w-[61px] shadow-[0px_1px_13px_rgba(216,229,237,0.18)]"
           />
         </div>
