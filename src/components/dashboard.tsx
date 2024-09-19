ecaf5d9b1b2090ffa48b36c8680369d9ae19791b
Abh1noob <abhinavganeshank@gmail.com>
2024-09-19 09:39:12 +0530

    fix: root page layout

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -4,10 +4,10 @@ import myImage from "@/assets/images/boxes.svg";
 import LeaderboardItem from "@/components/leaderboarditem";
 import TeamInfo from "@/components/teaminfo";
 import { type dashboardData } from "@/types/client/dashboard";
-import Image from "next/image";
-import { Button } from "./ui/button";
 import { UserIcon } from "lucide-react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
 
 interface DashboardProps {
   leaderboardData: dashboardData;
@@ -16,21 +16,26 @@ interface DashboardProps {
 const Dashboard: React.FC<DashboardProps> = ({ leaderboardData }) => {
   const router = useRouter();
   return (
-    <main className="mx-auto flex h-screen w-full max-w-[480px] flex-col items-center bg-white px-6 pt-12">
-      <div className="flex w-full items-center justify-between">
-        <div></div>
-        <h1 className="text-3xl font-semibold text-black">Dashboard</h1>
-        <Button
-          size="icon"
-          variant="ghost"
-          onClick={() => void router.push("/profile")}
-        >
-          <UserIcon />
-        </Button>
+    <main className="mx-auto flex h-full max-h-screen w-full max-w-[480px] flex-col items-center justify-between bg-white px-6 pt-8">
+      <div className="flex-1">
+        <div className="flex w-full items-center justify-between">
+          <div className="relative w-full">
+            <h1 className="text-center text-3xl font-semibold text-black">
+              Dashboard
+            </h1>
+            <Button
+              size="icon"
+              variant="ghost"
+              onClick={() => void router.push("/profile")}
+              className="absolute right-0 top-0"
+            >
+              <UserIcon />
+            </Button>
+          </div>
+        </div>
+        <TeamInfo data={leaderboardData} />
       </div>
-      <TeamInfo data={leaderboardData} />
-
-      <section className="mt-20 flex flex-col items-center text-zinc-800">
+      <section className="flex flex-col items-center text-zinc-800">
         <div className="grid w-full grid-cols-3">
           {leaderboardData.topTeams.slice(0, 3).map((item, index) => (
             <LeaderboardItem key={index} name={item.name} rank={index} />
